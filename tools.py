e1c0d432976f03071bc956e22e14139a220be27d
Jiujia <jiujia.zhang@gmail.com>
2023-08-27 18:20:26 -0400

    add noisy transitions

--- tools.py
+++ tools.py
@@ -15,4 +15,29 @@ def random_transitions(n_actions = 2, n_states = 2):
     return transitions
 
 
+def noisy_transitions(transition, sigma):
+
+    """
+    add noise to provided transition matrices
+    
+        @param transition: transition matrix p[a, s, s']
+        @param sigma:      noise level N(0, sigma)
+        
+        @return:
+            transition: perturbed transition matrix hat{p}[a, s, s']
+    """
+
+    n_actions, n_states = transition.shape[:-1]
+
+    for a in range(n_actions):
+        epsilon = np.random.normal(loc=0.0, scale=sigma, size=n_states**2)
+        epsilon = epsilon.reshape((n_states, n_states))
+
+        transition[a] += epsilon
+        transition[a] /= transition.sum(axis = 1, keepdims=True)
+
+    return transition
+
+
+
 
